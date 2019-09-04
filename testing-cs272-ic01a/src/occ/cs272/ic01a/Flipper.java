f9183989a8bdb46e1fb176c576d43840e26fe780
justi <justi@JJ-XPS13-LAPTOP.home>
2019-09-04 01:58:28 -0700

    initial commit onto local repo on my home laptop

---
+++ testing-cs272-ic01a/src/occ/cs272/ic01a/Flipper.java
@@ -0,0 +1,20 @@
+/**
+ * Experimenting with a from-scratch project:
+ * 1) Create a blank project in Eclipse. Add a class w main method.
+ * 2) Create a blank repo in GitHub with no README file or anything, i.e. completely blank. Copy it's URI link.
+ * 3) In Eclipse, Clone a Git Repository and add the clone to this view (Use the Git perspective.)
+ * 4) Right click on the Eclipse project in the Package Explorer or Navigator --> Team --> Share P
+ * 
+ * @author justi
+ * @version 9/4/2019
+ */
+package occ.cs272.ic01a;
+
+public class Flipper {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
